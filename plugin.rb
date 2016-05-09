0d078eb8d1167ee421f50f5f8a64584836e2f2f4
Dan Braghis <dan.braghis@torchbox.com>
2016-05-09 15:42:47 +0100

    Try getting params from request

--- plugin.rb
+++ plugin.rb
@@ -3,6 +3,11 @@
 # version: 0.1
 # authors: Dashamir Hoxha
 
+var params = request.query_parameters
+params.each do |key,value|
+  Rails.logger.warn "Param #{key}: #{value}"
+end
+
 if params[:embedded] == 'true'
   register_asset "javascript/iframeResizer.contentWindow.min.js", :server_side
 
